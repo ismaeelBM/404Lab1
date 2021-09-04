46bc0337299d8bff174b43cddf74882bafbe48f7
ismaeelBM <ismaeel@ualberta.ca>
2021-09-03 20:39:28 -0600

    Updated

--- requests_version.py
+++ requests_version.py
@@ -3,4 +3,7 @@ import requests
 print(requests.__version__)
 
 google_get = requests.get('https://www.google.com')
-print(google_get.status_code)
+
+git_get = requests.get('https://raw.githubusercontent.com/ismaeelBM/404Lab1/master/requests_version.py')
+
+print(git_get.text)
